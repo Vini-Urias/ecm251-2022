b8ce8f81e56ea23da3bde7a9c277477f55e386d6
Vini-Urias <76217672+Vini-Urias@users.noreply.github.com>
2022-06-09 10:49:15 -0300

    Delete Vinicius Urias directory

--- Vinicius Urias/EnumFuncoes.java
+++
@@ -1,3 +0,0 @@
-public enum EnumFuncoes {
-    MobileMembers, HeavyLifters, ScriptGuys, BigBrothers
-}
--- Vinicius Urias/IApresentacao.java
+++
@@ -1,4 +0,0 @@
-public interface IApresentacao {
-    public abstract void apresentar();
-    
-}
